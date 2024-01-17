d77ba35918d97eac5c58d837187bdb8444fc4166
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-01-17 11:50:45 +0100

    [DB/PHP] Added more implementation of database.php, it is missing only some little correction (do some method or no)

--- src/db/database.php
+++ src/db/database.php
@@ -13,27 +13,27 @@ class DatabaseHelper {
      * User CRUD
      */
 
-    public function getUserById($userName) {
+    public function getUserById($username) {
         $query = "
-            SELECT userName, imgProfilo, nome, cognome, email, dataNascita, codCensismento, 
+            SELECT username, imgProfilo, nome, cognome, email, dataNascita, codCensismento, 
                 gruppoApp, pw, scout, bio, fazzolettone, specialità, totem 
             FROM utente 
-            WHERE userName = ?
+            WHERE username = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $userName);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_assoc(MYSQLI_ASSOC);
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
     public function serchUser($input) {
         $query = "
-            SELECT userName, imgProfilo, nome, cognome,
+            SELECT username, imgProfilo, nome, cognome,
             FROM utente 
-            WHERE userName LIKE CONCAT(?, '%') 
+            WHERE username LIKE CONCAT(?, '%') 
             OR nome LIKE CONCAT(?, '%') 
             OR cognome LIKE CONCAT(?, '%')
         ";
@@ -46,15 +46,15 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getUsersByUsername($slug) {
+    public function getUsersByusername($username) {
         $query = "
-            SELECT userName, imgProfilo
+            SELECT username, imgProfilo
             FROM utente 
-            WHERE userName LIKE ?
+            WHERE username LIKE ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $slug);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -62,112 +62,112 @@ class DatabaseHelper {
     }
 
     /** Funzione che non so se andremo ad usare, MOMENTANEA*/
-    public function getUsersFriendsById ($userName) {
+    public function getUsersFriendsById ($username) {
         $query = "
-            SELECT u.userName, u.imgProfilo
-            FROM seguire s INNER JOIN utente u ON s.userNameSeguito = u.userName
-            WHERE s.userNameSeguace = ?
+            SELECT u.username, u.imgProfilo
+            FROM seguire s INNER JOIN utente u ON s.usernameSeguito = u.username
+            WHERE s.usernameSeguace = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $userName);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getNotificationsById($userName) {
+    public function getNotificationsById($username) {
         $query = "
             SELECT *
             FROM Notifica
-            WHERE userName = ? AND letta = false
+            WHERE username = ? AND letta = false
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $userName);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getSeguitiById($userName) {
+    public function getSeguitiById($username) {
         $query = "
-            SELECT u.userNameSeguito, u.imgProfilo, u.nome, u.cognome
-            FROM seguire s INNER JOIN utente u ON s.userNameSeguito = u.userName
-            WHERE s.userNameSeguace = ?
+            SELECT u.usernameSeguito, u.imgProfilo, u.nome, u.cognome
+            FROM seguire s INNER JOIN utente u ON s.usernameSeguito = u.username
+            WHERE s.usernameSeguace = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $userName);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getSeguaciById($userName) {
+    public function getSeguaciById($username) {
         $query = "
-            SELECT u.userNameSeguace, u.imgProfilo, u.nome, u.cognome
-            FROM seguire s INNER JOIN utente u ON s.userNameSeguace = u.userName
-            WHERE s.userNameSeguito = ?
+            SELECT u.usernameSeguace, u.imgProfilo, u.nome, u.cognome
+            FROM seguire s INNER JOIN utente u ON s.usernameSeguace = u.username
+            WHERE s.usernameSeguito = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $userName);
+        $stmt->bind_param("s", $username);
         $stmt->execute();
         $result = $stmt->get_result();
 
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function checkFollow($userName, $userNameFollowd) {
+    public function checkFollow($username, $usernameFollowed) {
         $query = "
             SELECT *
             FROM seguire
-            WHERE userName = ? AND userNameFollowed = ?
+            WHERE username = ? AND usernameFollowed = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ss", $userName, $userNameFollowed);
+        $stmt->bind_param("ss", $username, $usernameFollowed);
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_assoc(MYSQLI_ASSOC);
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function follow($userName, $userNameFollowed) {
+    public function follow($username, $usernameFollowed) {
         $query = "
-            INSERT INTO seguire (userName, userNameFollowed)
+            INSERT INTO seguire (username, usernameFollowed)
             VALUES (?, ?)
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ss", $userName, $userNameFollowed);
+        $stmt->bind_param("ss", $username, $usernameFollowed);
         $stmt->execute();
     }
 
-    public function unfollow($userName, $userNameFollowed) {
+    public function unfollow($username, $usernameFollowed) {
         $query = "
             DELETE FROM seguire
-            WHERE userName = ? AND userNameFollowed = ?
+            WHERE username = ? AND usernameFollowed = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ss", $userName, $userNameFollowed);
+        $stmt->bind_param("ss", $username, $usernameFollowed);
         $stmt->execute();
     }
 
-    public function updateUserWithoutImg($username, $email, $nome, $cognome) {
+    public function updateUserWithoutImg($username, $email, $name, $surname) {
         $query = "
             UPDATE utente
             SET email = ?, nome = ?, cognome = ?
-            WHERE userName = ?
+            WHERE username = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ssss", $email, $nome, $cognome, $username);
+        $stmt->bind_param("ssss", $email, $name, $surname, $username);
         $stmt->execute();
     }
 
@@ -177,8 +177,8 @@ class DatabaseHelper {
 
     public function getPostById($idPost) {
         $query = "
-            SELECT u.userName, u.imgProfilo, u.nome, u.cognome, p.idPost, p.dataOra, p.testo, p.imgPost, p.like, p.commenti
-            FROM post p INNER JOIN utente u ON p.userName = u.userName INNER JOIN hashtag h ON p.hastag = h.idHastag
+            SELECT u.username, u.imgProfilo, u.nome, u.cognome, p.idPost, p.dataOra, p.testo, p.imgPost, p.like, p.commenti
+            FROM post p INNER JOIN utente u ON p.username = u.username INNER JOIN hashtag h ON p.hastag = h.idHastag
             WHERE p.idPost = ?
             WHERE idPost = ?
         ";
@@ -188,18 +188,18 @@ class DatabaseHelper {
         $stmt->execute();
         $result = $stmt->get_result();
 
-        return $result->fetch_assoc(MYSQLI_ASSOC);
+        return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function getPostByhashtag ($nomeHashtag) {
+    public function getPostByhashtag ($hashtagName) {
         $query = "
-            SELECT u.userName, u.imgProfilo, u.nome, u.cognome, p.idPost, p.dataOra, p.testo, p.imgPost, p.like, p.commenti
-            FROM post p INNER JOIN utente u ON p.userName = u.userName INNER JOIN hashtag h ON p.hastag = h.idHastag
+            SELECT u.username, u.imgProfilo, u.nome, u.cognome, p.idPost, p.dataOra, p.testo, p.imgPost, p.like, p.commenti
+            FROM post p INNER JOIN utente u ON p.username = u.username INNER JOIN hashtag h ON p.hastag = h.idHastag
             WHERE h.nome = ?
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("s", $nomeHashtag);
+        $stmt->bind_param("s", $hashtagName);
         $stmt->execute();
         $result = $stmt->get_result();
 
@@ -208,20 +208,20 @@ class DatabaseHelper {
 
     /*METTERE UNA FUNZIONE CHE restituisce i dati del giorno e mese passati come parametro ??? */
 
-    public function insertPost($idPost, $immagine, $hashtag, $utente) {
+    public function insertPost($idPost, $image, $hashtag, $username) {
         $query = "
-            INSERT INTO post (idPost, imgPost, hastag, userName)
+            INSERT INTO post (idPost, imgPost, hastag, username)
             VALUES (?, ?, ?, ?)
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("isss", $idPost, $immagine, $hashtag, $utente);
+        $stmt->bind_param("isss", $idPost, $image, $hashtag, $username);
         $stmt->execute();
 
         return $stmt->insert_id;
     }
 
-    public function updatePostWhitImage($idPost, $testo, $immagine) {
+    public function updatePostWhitImage($idPost, $text, $image) {
         $query = "
             UPDATE post
             SET testo = ?, imgPost = ?
@@ -229,13 +229,13 @@ class DatabaseHelper {
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("ssi", $testo, $immagine, $idPost);
+        $stmt->bind_param("ssi", $text, $image, $idPost);
         $stmt->execute();
 
         return $stmt->execute();
     }
 
-    public function updatePostWithoutImage($idPost, $testo) {
+    public function updatePostWithoutImage($idPost, $text) {
         $query = "
             UPDATE post
             SET testo = ?
@@ -243,13 +243,13 @@ class DatabaseHelper {
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("si", $testo, $idPost);
+        $stmt->bind_param("si", $text, $idPost);
         $stmt->execute();
 
         return $stmt->execute();
     }
 
-    public function deletePostimage($idPost) {
+    public function deletePostImage($idPost) {
         $query = "
             UPDATE post
             SET immagine = NULL
@@ -328,8 +328,9 @@ class DatabaseHelper {
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("i", $idPost);
         $stmt->execute();
+        var_dump($stmt->error); //is used for debugging purposes. It will output any error message from the last operation on $stmt.
 
-        return $stmt->execute();
+        return true;
     }
 
     /**
@@ -338,8 +339,8 @@ class DatabaseHelper {
 
     public function getCommentsById($idPost) {
         $query = "
-            SELECT u.userName, u.imgProfilo, u.nome, u.cognome, c.idCommento, c.dataOra, c.testo
-            FROM commento c INNER JOIN utente u ON c.userName = u.userName
+            SELECT u.username, u.imgProfilo, u.nome, u.cognome, c.idCommento, c.dataOra, c.testo
+            FROM commento c INNER JOIN utente u ON c.username = u.username
             WHERE c.idPost = ?
         ";
 
@@ -351,14 +352,14 @@ class DatabaseHelper {
         return $result->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function insertComment($idCommento, $testo, $utente, $idPost, $idNotifica) {
+    public function insertComment($idCommento, $text, $username, $idPost, $idNotifitication) {
         $query = "
-            INSERT INTO commento (idCommento, testo, userName, idPost, idNotifica)
+            INSERT INTO commento (idCommento, testo, username, idPost, idNotifica)
             VALUES (?, ?, ?, ?)
         ";
 
         $stmt = $this->db->prepare($query);
-        $stmt->bind_param("issi", $idCommento, $testo, $utente, $idPost, $idNotifica);
+        $stmt->bind_param("issii", $idCommento, $text, $username, $idPost, $idNotification);
         $stmt->execute();
 
         return $stmt->insert_id;
@@ -368,5 +369,136 @@ class DatabaseHelper {
      * Likes CRUD
      */
 
-    
+    public function getLikesByPostId($idPost) {
+        $query = "
+            SELECT like
+            FORM post
+            WHERE idPost = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $idPost);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function getLikesByUserAndPostId($username, $idPost) {
+        $query = "
+            SELECT *
+            FROM like
+            WHERE username = ? AND idPost = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("si", $username, $idPost);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function insertLike($idPost, $username) {
+        $query = "
+            INSERT INTO like (idPost, username)
+            VALUES (?, ?)
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("is", $idPost, $username);
+        $stmt->execute();
+        $result = array("username" => $username, "idPost" => $idPost);
+
+        return $result;
+    }
+
+    public function removelike($idPost, $username) {
+        $query = "
+            DELETE FROM like
+            WHERE idPost = ? AND username = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("is", $idPost, $username);
+        $stmt->execute();
+
+        return $stmt->execute();
+    }
+
+    /**
+     * Notifications CRUD
+     */
+
+    public function insertNotification($testo, $idPost, $usernameReciver, $usernameSender) {
+        $query = "
+            INSERT INTO notifica (testo, idPost, usernameReciver, usernameSender)
+            VALUES (?, ?, ?, ?)
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("siss", $testo, $idPost, $usernameReciver, $usernameSender);
+        $stmt->execute();
+
+        return $stmt->insert_id;
+    }
+
+    public function removeNotification($idNotifica) {
+        $query = "
+            DELETE FROM notifica
+            WHERE idNotifica = ?
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("i", $idNotifica);
+        $stmt->execute();
+
+        return $stmt->execute();
+    }
+
+    /**
+     * Login
+     */
+
+    public function checkLogin($username, $password) {
+        $query = "
+            SELECT *
+            FROM utente
+            WHERE username = ? AND password = ? LIMIT 1
+        ";
+
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ss", $username, $password);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
+
+    //funzione che inserisce un tentativo di login
+    public function insertLoginAttempt($username, $time){
+        //todo
+    }
+
+    //Funzione che restituisce se il tentativo di login è fallito o meno
+    public function getLoginAttempt($username, $timeThd){
+        //todo
+    }
+
+    /**
+     * Register
+     */
+
+    public function insertUser($username, $nome, $cognome, $dataNascita, $codCensimento, $gruppo, $email, $password, $scout, $bio, $fazzolettone, $specialita, $totem){
+        $query = "
+            INSERT INTO utente (username, nome, cognome, dataNascita, codCensimento, gruppo, email, password, scout, bio, fazzolettone, specialita, totem)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ssssisssissss", $username, $nome, $cognome, $dataNascita, $codCensimento, $gruppo, $email, $password, $scout, $bio, $fazzolettone, $specialita, $totem);
+        $stmt->execute();
+
+        return $username;
+    }
+
 }
