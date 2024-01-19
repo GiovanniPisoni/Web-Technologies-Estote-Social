5b9b6c7d3d1263c62518d9a28dda3639410cd9ba
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-01-19 18:52:05 +0100

    [dev] Added login page

---
+++ src/template/login.php
@@ -0,0 +1,18 @@
+<form action="#" method="POST" id="login-Form" class="position-relative">
+    <div class="d-flex justify-content-center">
+        <img src="img/LoginPhoto.png" alt="Login photo" width="275"/>
+    </div>
+    <div class="d-flex justify-content-center mt-3"> 
+        <label for="username" class="me-2">Username</label>
+        <input type="text" name="username" id="username" placeholder="Username" required>
+    </div>
+    <div class="d-flex justify-content-center mt-3">
+        <label for="password" class="me-2">Password</label>
+        <input type="password" name="password" id="password" placeholder="Password" required>
+    </div>
+    <div class="d-flex justify-content-center">
+        <label for="login" hidden>Login</label>
+        <input class="btn btn-success w-40 mt-3 mb-2" type="submit" name="login" id="login" value="Login">
+    </div>
+    <p class="text-center">Non Hai ancora un account? Beh.. <a href="register.php" class="text-success">REGISTRATI</a></p>     
+</form>
--- src/template/login_base.php
+++ src/template/login_base.php
@@ -1,2 +1,26 @@
-<?php
-?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <title>EstoteSocial: <?php echo $templateParams["title"]; ?></title>
+        <link rel="icon" type="image/x-icon" href="img/logo.png">
+    </head>
+    <body class="d-flex justify-content-center py-4 bg-success-subtle">
+        <style>
+            .bg-light-brown {background-color: #e3d2bc;}
+        </style>
+        <div class="mw-75 p-3 mb-2 bg-light-brown rounded">
+            <header>
+                <div class="d-flex justify-content-center">
+                    <img src="img/logo.png" alt="EstoteSocial icon" width="250"/>
+                </div>
+                <h4 class=" my-0 mb-3 fw-normal m-10 text-center"><?php echo $templateParams["title"]; ?></h4>
+            </header>
+            <main>
+                <?php require($templateParams["name"]); ?>
+            </main>
+        </div>
+    </body>
+</html>
