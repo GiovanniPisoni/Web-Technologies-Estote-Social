9e88187322e9284ac2554ee88ce0f3f1d1d9343d
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2024-01-20 19:59:30 +0100

    [PHP] Implemented cercaHashtag-api.php

---
+++ src/api/cercaHashtag-api.php
@@ -0,0 +1,15 @@
+<?php
+    require_once("db_config.php");
+
+    $username = $_POST["username"];
+
+    //controlla se esiste un particolare hahshtag, in caso esista ritorna tutti i post con quel hashtag
+    $result = $dbh->getPostByHashtag($username);
+
+    if(empty($result)){
+        $result = false;
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($result);
+?>
