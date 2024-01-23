8a2cc928671fcc8b4b18acb1fd27a07faf519aa8
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-01-23 16:25:37 +0100

    [dev] Added minor changes on base-homepage.php

--- src/template/base-homepage.php
+++ src/template/base-homepage.php
@@ -20,7 +20,7 @@
         ?>-->
     </head>
     <body class="container justify-content-center py-4 bg-success-subtle">
-        <nav class="nav p-2 fixed-top">
+        <nav class="nav p-1 fixed-top">
             <div class="container">
                <div class="row">
                     <div class="col text-start d-flex justify-content-center align-items-center me-3">
@@ -32,19 +32,20 @@
                                 $iconPath = "./img/backpack.png";
                             }
                         ?>
-                        <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-                            <image href="<?php echo $iconPath; ?>" height="40" width="40"/>
+                        <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
+                            <image href="<?php echo $iconPath; ?>" height="35" width="35"/>
                         </svg>
                     </div>
                     <div class="col text-center pos-relative d-flex justify-content-center align-items-center">
-                        <img src="./img/miniLogo_white.png" alt="EstoteSocial" height="40">
+                        <img src="./img/miniLogo_white.png" alt="EstoteSocial" height="35">
                     </div>
                     <div class="col text-end pos-realtive d-flex justify-content-center align-items-center ms-3">
-                        <svg data-bs-toggle="modal" data-bs-target="#new-post-modal" width="30" height="30" viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
+                        <svg data-bs-toggle="modal" data-bs-target="#new-post-modal" width="35" height="35" viewBox="0 -0.5 25 25" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
                             <g id="SVGRepo_bgCarrier" stroke-width="0"/>
                             <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"/>
                             <g id="SVGRepo_iconCarrier">    
-                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.26801 18.1003L5.75301 12.8133C5.41566 12.2907 5.41566 11.6189 5.75301 11.0963L8.25201 5.89229C8.63454 5.32445 9.27844 4.98876 9.96301 5.00029H13.002H16.041C16.7256 4.98876 17.3695 5.32445 17.752 5.89229L20.246 11.0923C20.5834 11.6149 20.5834 12.2867 20.246 12.8093L17.736 18.1003C17.3537 18.674 16.7053 19.0133 16.016 19.0003H9.98701C9.29805 19.013 8.65011 18.6737 8.26801 18.1003Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/> <path d="M11.002 11.2502C10.5878 11.2502 10.252 11.586 10.252 12.0002C10.252 12.4145 10.5878 12.7502 11.002 12.7502V11.2502ZM13.002 12.7502C13.4162 12.7502 13.752 12.4145 13.752 12.0002C13.752 11.586 13.4162 11.2502 13.002 11.2502V12.7502ZM13.002 11.2502C12.5878 11.2502 12.252 11.586 12.252 12.0002C12.252 12.4145 12.5878 12.7502 13.002 12.7502V11.2502ZM15.002 12.7502C15.4162 12.7502 15.752 12.4145 15.752 12.0002C15.752 11.586 15.4162 11.2502 15.002 11.2502V12.7502ZM13.752 12.0002C13.752 11.586 13.4162 11.2502 13.002 11.2502C12.5878 11.2502 12.252 11.586 12.252 12.0002H13.752ZM12.252 14.0002C12.252 14.4145 12.5878 14.7502 13.002 14.7502C13.4162 14.7502 13.752 14.4145 13.752 14.0002H12.252ZM12.252 12.0002C12.252 12.4145 12.5878 12.7502 13.002 12.7502C13.4162 12.7502 13.752 12.4145 13.752 12.0002H12.252ZM13.752 10.0002C13.752 9.58603 13.4162 9.25024 13.002 9.25024C12.5878 9.25024 12.252 9.58603 12.252 10.0002H13.752ZM11.002 12.7502H13.002V11.2502H11.002V12.7502ZM13.002 12.7502H15.002V11.2502H13.002V12.7502ZM12.252 12.0002V14.0002H13.752V12.0002H12.252ZM13.752 12.0002V10.0002H12.252V12.0002H13.752Z" fill="#ffffff"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M8.26801 18.1003L5.75301 12.8133C5.41566 12.2907 5.41566 11.6189 5.75301 11.0963L8.25201 5.89229C8.63454 5.32445 9.27844 4.98876 9.96301 5.00029H13.002H16.041C16.7256 4.98876 17.3695 5.32445 17.752 5.89229L20.246 11.0923C20.5834 11.6149 20.5834 12.2867 20.246 12.8093L17.736 18.1003C17.3537 18.674 16.7053 19.0133 16.016 19.0003H9.98701C9.29805 19.013 8.65011 18.6737 8.26801 18.1003Z" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M11.002 11.2502C10.5878 11.2502 10.252 11.586 10.252 12.0002C10.252 12.4145 10.5878 12.7502 11.002 12.7502V11.2502ZM13.002 12.7502C13.4162 12.7502 13.752 12.4145 13.752 12.0002C13.752 11.586 13.4162 11.2502 13.002 11.2502V12.7502ZM13.002 11.2502C12.5878 11.2502 12.252 11.586 12.252 12.0002C12.252 12.4145 12.5878 12.7502 13.002 12.7502V11.2502ZM15.002 12.7502C15.4162 12.7502 15.752 12.4145 15.752 12.0002C15.752 11.586 15.4162 11.2502 15.002 11.2502V12.7502ZM13.752 12.0002C13.752 11.586 13.4162 11.2502 13.002 11.2502C12.5878 11.2502 12.252 11.586 12.252 12.0002H13.752ZM12.252 14.0002C12.252 14.4145 12.5878 14.7502 13.002 14.7502C13.4162 14.7502 13.752 14.4145 13.752 14.0002H12.252ZM12.252 12.0002C12.252 12.4145 12.5878 12.7502 13.002 12.7502C13.4162 12.7502 13.752 12.4145 13.752 12.0002H12.252ZM13.752 10.0002C13.752 9.58603 13.4162 9.25024 13.002 9.25024C12.5878 9.25024 12.252 9.58603 12.252 10.0002H13.752ZM11.002 12.7502H13.002V11.2502H11.002V12.7502ZM13.002 12.7502H15.002V11.2502H13.002V12.7502ZM12.252 12.0002V14.0002H13.752V12.0002H12.252ZM13.752 12.0002V10.0002H12.252V12.0002H13.752Z" fill="#ffffff"/>
                             </g>
                         </svg>
                     </div>
@@ -52,27 +53,27 @@
             </div>
         </nav>
         <div class="d-flex fixed-bottom justify-content-center">
-            <nav class="nav d-flex fixed-bottom">
+            <nav class="nav p-1 d-flex fixed-bottom">
                 <div class="container">
                     <div class="row">
                         <div class="col text-start d-flex justify-content-center align-items-center me-3">
                             <a href="search.php" class="link-underline link-underline-opacity-0">
-                                <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-                                    <image href="./img/find.png" height="40" width="40"/>
+                                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="./img/find.png" height="35" width="35"/>
                                 </svg>
                             </a>
                         </div>
                         <div class="col text-start d-flex justify-content-center align-items-center"> 
                             <a href="index.php" class="link-underline link-underline-opacity-0">
-                                <svg width="40" height="40" xmlns="http://www.w3.org/2000/svg">
-                                    <image href="./img/tent.png" height="40" width="40"/>
+                                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="./img/tent.png" height="35" width="35"/>
                                 </svg>
                             </a>
                         </div>
                         <div class="col text-start d-flex justify-content-center align-items-center me-3">
                             <a href="profile.php?id=<?php echo $_SESSION["username"]; ?>" class="link-underline link-underline-opacity-0">
-                                <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
-                                    <image href="./img/user.png" height="35" width="35"/>
+                                <svg width="32.5" height="32.5" xmlns="http://www.w3.org/2000/svg">
+                                    <image href="./img/user.png" height="32.5" width="32.5"/>
                                 </svg>
                             </a>
                         </div>
