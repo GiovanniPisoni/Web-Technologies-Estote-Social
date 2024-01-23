15ea200c601e95e90e72c12179b1588dade627aa
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2024-01-23 13:54:24 +0100

    Fixed login to homepage

--- src/js/login.js
+++ src/js/login.js
@@ -11,7 +11,7 @@ function login() {
     axios.post('./api/login-api.php', formDB).then(response => {
         if (response.data["login_result"]) {
             document.getElementById("result").innerText = "Login avvenuto con successo!!";
-            document.location.href = "./template/base-homepage.php";
+            document.location.href = "./";
         } else {
             document.getElementById("result").innerText = response.data["login_error"];
         }
