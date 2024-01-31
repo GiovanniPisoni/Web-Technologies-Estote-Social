8adb2a9b654744dac935e59019f9a9c11676b95d
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-01-31 09:18:02 +0100

    [dev] minor changes

--- src/db_config.php
+++ src/db_config.php
@@ -4,6 +4,7 @@
     $dbh = new DatabaseHelper("localhost", "root", "", "db_estotesocial", "3306");
     define("UPLOAD_DIR", "img/");
 
-    if(!isset($_SESSION))
+    if(!isset($_SESSION)) {
         session_start_withCookies();
+    }
 ?>
