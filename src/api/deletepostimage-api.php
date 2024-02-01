3b45aef011a65a457f7840667f359d3ad66e1d15
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2024-02-01 12:05:57 +0100

    Fixed modify post implementation

--- src/api/deletepostimage-api.php
+++ src/api/deletepostimage-api.php
@@ -1,11 +1,6 @@
 <?php 
     require_once("../db_config.php");
 
-   //redirect if not auth
-   if(!userIsAlreadyIn($dbh->db)){
-        header('Location: ../index.php');
-    }
-
    $idPost = $_POST["idPost"];
 
    //elimina una determinata immagine dal file system (img post)
