13e2c55d7aa5989ee52f90d025aebad25d41e79b
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-02-06 16:47:24 +0100

    Added accessibility corrections

--- src/template/base-homepage.php
+++ src/template/base-homepage.php
@@ -66,21 +66,21 @@
                         <div class="col text-start d-flex justify-content-center align-items-center me-3">
                             <a href="search.php" class="link-underline link-underline-opacity-0">
                                 <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
-                                    <image href="./img/find.png" height="35" width="35"/>
+                                    <image href="./img/find.png" height="35" width="35" alt="serchIcon"/>
                                 </svg>
                             </a>
                         </div>
                         <div class="col text-start d-flex justify-content-center align-items-center"> 
                             <a href="index.php" class="link-underline link-underline-opacity-0">
                                 <svg width="35" height="35" xmlns="http://www.w3.org/2000/svg">
-                                    <image href="./img/tent.png" height="35" width="35"/>
+                                    <image href="./img/tent.png" height="35" width="35" alt="homeIcon"/>
                                 </svg>
                             </a>
                         </div>
                         <div class="col text-start d-flex justify-content-center align-items-center ms-3">
                             <a href="profile.php?username=<?php echo $_SESSION["username"]; ?>" class="link-underline link-underline-opacity-0">
                                 <svg width="32.5" height="32.5" xmlns="http://www.w3.org/2000/svg">
-                                    <image href="./img/user.png" height="32.5" width="32.5"/>
+                                    <image href="./img/user.png" height="32.5" width="32.5" alt="profileIcon"/>
                                 </svg>
                             </a>
                         </div>
